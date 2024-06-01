0f87b9741846bcf67289afbdc6e82f9ba846ae39
onepisYa <pis1@qq.com>
2024-06-01 21:22:35 +0800

    fix: fix typescript type

--- src/pages/onepisya/use-reducer/examples/UseImmerTodo.tsx
+++ src/pages/onepisya/use-reducer/examples/UseImmerTodo.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 by onepisYa pis1@qq.com , All Rights Reserved.
  * @Date: 2024-06-01 04:08:10
  * @LastEditors: onepisYa pis1@qq.com
- * @LastEditTime: 2024-06-01 04:17:31
+ * @LastEditTime: 2024-06-01 21:22:09
  * @FilePath: /parctice-react-ant-design-pro/src/pages/onepisya/use-reducer/examples/UseImmerTodo.tsx
  * 路漫漫其修远兮，吾将上下而求索。
  * @Description:
@@ -13,7 +13,8 @@ import type { Task, TaskAction } from './todo/index';
 import styles from './todo/styles.less';
 import TaskList from './todo/TaskList';
 
-function tasksReducer(draft: Task[], action: TaskAction) {
+function tasksReducer(draft: Task[]|null, action: TaskAction):Task[]|null{
+  if (draft===null){return null}
   switch (action.type) {
     case 'added': {
       draft.push({
@@ -21,12 +22,14 @@ function tasksReducer(draft: Task[], action: TaskAction) {
         text: action.text,
         done: false,
       } as Task);
-      break;
+      // break;
+      return null
     }
     case 'changed': {
-      const index = draft.findIndex((t) => t.id === action?.task?.id);
+      const index = draft?.findIndex((t) => t.id === action?.task?.id);
       draft[index] = action.task!;
-      break;
+      // break;
+      return null
     }
     case 'deleted': {
       return draft.filter((t) => t.id !== action.id);
@@ -38,7 +41,7 @@ function tasksReducer(draft: Task[], action: TaskAction) {
 }
 
 export default function TaskApp() {
-  const [tasks, dispatch] = useImmerReducer<Task[]>(tasksReducer, initialTasks);
+  const [tasks, dispatch] = useImmerReducer<Task[]|null, TaskAction>(tasksReducer, initialTasks);
 
   function handleAddTask(text: Task['text']) {
     dispatch({
@@ -67,7 +70,7 @@ export default function TaskApp() {
       <div className={styles['onepisya-todo']}>
         <h1>Prague itinerary</h1>
         <AddTask onAddTask={handleAddTask} />
-        <TaskList tasks={tasks} onChangeTask={handleChangeTask} onDeleteTask={handleDeleteTask} />
+        <TaskList tasks={tasks!} onChangeTask={handleChangeTask} onDeleteTask={handleDeleteTask} />
       </div>
     </>
   );
